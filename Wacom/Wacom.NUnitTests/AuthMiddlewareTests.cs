60f058140ee3bace4bc0402c2e01d2a37a2b4037
Ivo Ivanov <ivo.ivanov@flatrocktech.com>
2024-04-30 14:31:33 +0300

    add tests for auth and file controller

---
+++ Wacom/Wacom.NUnitTests/AuthMiddlewareTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Moq;
+using System.ComponentModel.DataAnnotations;
+using WacomAPI.Middleware;
+using WacomCore.Contracts;
+using WacomCore.Settings;
+
+namespace Wacom.NUnitTests
+{
+	public class AuthMiddlewareTests
+	{
+		private Mock<RequestDelegate> requestDelegate;
+		private DefaultHttpContext context;
+
+		[SetUp]
+		public void Setup()
+		{
+			this.requestDelegate = new Mock<RequestDelegate>();
+			this.context = new DefaultHttpContext();
+		}
+
+		[Test]
+		public async Task Invoke_WhenTokenheaderIsNotBearer_ThrowError()
+		{
+			//Arrange
+			var userService = new Mock<IUserService>();
+			var jwtSettingsMock = new Mock<IOptions<JWTSettings>>();
+			var jwtSettings = new JWTSettings() { Type = "Bearer" };
+			jwtSettingsMock.Setup(x => x.Value).Returns(jwtSettings);
+			var noBearer = "\"NoBearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQ2Y2M1MWViLWRjOTEtNGYyYy1hZmYyLTVkMTE1MTk3MDhlMyIsIm5iZiI6MTcxNDQ2OTQ3OCwiZXhwIjoxNzE0NDczMDc4LCJpYXQiOjE3MTQ0Njk0Nzh9.7qIOdEiHkGxIv5OPQA_9_9Er8LtJ5YwRCVlZCFrbmhA\"";
+			this.context.Request.Headers.Add("Authorization", noBearer);
+			var authMiddleware = new AuthMiddleware(jwtSettingsMock.Object,
+				userService.Object);
+
+			//Act
+			var result = Assert.ThrowsAsync<ValidationException>(() =>
+				authMiddleware.InvokeAsync(this.context, this.requestDelegate.Object));
+
+			//Assert
+			Assert.That(result.Message, Is.Not.Null);
+			Assert.That(result.Message, Is.EqualTo("Error Authentication."));
+		}
+	}
+}
---
+++ Wacom/Wacom.NUnitTests/FileControllerTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Moq;
+using WacomAPI.Controllers;
+using WacomCore.Contracts;
+using WacomCore.Settings;
+
+namespace Wacom.NUnitTests
+{
+	public class FileControllerTests
+	{
+		private FilesController fileController;
+		private Mock<IOptions<FileSettings>> fileSettingsMock;
+		private int fileSize = 1000;
+		[SetUp]
+		public void Setup()
+		{
+			this.fileSettingsMock = new Mock<IOptions<FileSettings>>();
+			var fileServiceMock = new Mock<IFileService>();
+			var jwtSettings = new FileSettings() { FileSize = fileSize };
+			fileSettingsMock.Setup(x => x.Value).Returns(jwtSettings);
+			this.fileController = new FilesController(fileSettingsMock.Object,
+				fileServiceMock.Object);
+		}
+
+		[Test]
+		public async Task PostFile_WhenFileIsNull_ReturnBadReq()
+		{
+			//Act
+			var response = await fileController.Post(null);
+
+			//Assert
+			Assert.IsInstanceOf<BadRequestObjectResult>(response);
+			var badReq = (BadRequestObjectResult)response;
+			Assert.That(badReq, Is.Not.Null);
+			Assert.That(badReq.StatusCode, Is.EqualTo(400));
+		}
+
+		[Test]
+		public async Task PostFile_WhenFileIsSizeIsLarger_ReturnBadReq()
+		{
+			//Arrange
+			var stream = new MemoryStream();
+			var file = new FormFile(
+				stream, 0, fileSize+1, "id_from_form", "");
+
+			//Act
+			var response = await fileController.Post(file);
+
+			//Assert
+			Assert.IsInstanceOf<BadRequestObjectResult>(response);
+			var badReq = (BadRequestObjectResult)response;
+			Assert.That(badReq, Is.Not.Null);
+			Assert.That(badReq.StatusCode, Is.EqualTo(400));
+			Assert.That(badReq.Value, Is.EqualTo($"File is too large. Maximum of {this.fileSize}"));
+		}
+	}
+}
